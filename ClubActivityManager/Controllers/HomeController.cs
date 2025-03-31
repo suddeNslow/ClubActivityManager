789f73195bb76ea43102e8620d4de744bcaf54b6
Bogdan-Gabriel Valceanu <125970174+suddeNslow@users.noreply.github.com>
2025-03-31 22:21:28 +0300

    Update HomeController.cs
    test nu merge

--- ClubActivityManager/Controllers/HomeController.cs
+++ ClubActivityManager/Controllers/HomeController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using ClubActivityManager.Models;
 
 namespace ClubActivityManager.Controllers;
-
+ciocan
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
